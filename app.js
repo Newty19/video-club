6319c1b35a2c30c56bdba1f15b39cde8e948fa19
Eduardo Rodriguez <a334144@uach.mx>
2023-03-04 00:12:07 -0600

    DB agregado

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/adress',adressRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404 ));
 });
 
 // error handler
